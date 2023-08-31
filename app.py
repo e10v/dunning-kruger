ed2b6736b8711d9d03927aa1c413f2f9291476b2
Evgeny Ivanov <ivanov.evgeny.n@gmail.com>
2023-09-01 01:47:53 +0600

    Improve visualizations

--- app.py
+++ app.py
@@ -57,13 +57,14 @@ def create_quartile_chart(data: pl.DataFrame, quartile_col: str) -> alt.Chart:
         )
         .group_by(quartile_col)
         .mean()
-        .melt(id_vars=quartile_col)
+        .melt(id_vars=quartile_col, value_name="average")
         .pipe(alt.Chart)
         .mark_line(point=True)
         .encode(
+            alt.Color("variable:N").title(None)
+                .sort(("test_score_percentile", "perceived_ability_percentile")),
             alt.X(f"{quartile_col}:O").sort(QUARTILES),
-            alt.Y("value:Q").title(None),
-            alt.Color("variable:N").title(None),
+            alt.Y("average:Q").title(None),
         )
     )
 
@@ -84,7 +85,7 @@ if __name__ == "__main__":
         label="Number of participants",
         min_value=100,
         max_value=500,
-        value=100,
+        value=300,
         step=20,
     )
 
@@ -97,16 +98,22 @@ if __name__ == "__main__":
     )
 
     st.header("Test score vs. perceived ability")
-    st.altair_chart(create_percentile_chart(data), use_container_width=True)
+    st.altair_chart(
+        create_percentile_chart(data),
+        use_container_width=True,
+        theme=None,
+    )
 
     st.header("Average percentiles by test score quartiles")
     st.altair_chart(
         create_quartile_chart(data, "test_score_quartile"),
         use_container_width=True,
+        theme=None,
     )
 
     st.header("Average percentiles by perceived ability quartiles")
     st.altair_chart(
         create_quartile_chart(data, "perceived_ability_quartile"),
         use_container_width=True,
+        theme=None,
     )
